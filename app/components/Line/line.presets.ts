b91eea04d8273d3bc9b638f260fbb7dafb5873b6
Reykjavik151 <Reykjavik151@gmail.com>
2020-11-18 21:45:59 +0200

    reafctor: added LINE_HEIGHT constants

---
+++ app/components/Line/line.presets.ts
@@ -0,0 +1,7 @@
+export const LINE_HEIGHT = {
+  SMALL: 12,
+  MEDIUM: 18,
+  LARGE: 32,
+  VERY_LARGE: 42,
+  BUTTON: 64,
+};
--- app/screens/Welcome/index.tsx
+++ app/screens/Welcome/index.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useCallback } from 'react';
 import { SafeAreaView, View, TouchableOpacity, Text } from 'react-native';
 
 import { Line } from '../../components/Line';
+import { LINE_HEIGHT } from '../../components/Line/line.presets';
 import { COMMON_STYLES } from '../../utils/commonStyles';
 import { WelcomeScreenProps } from './initial.props';
 import { styles } from './initial.styles';
@@ -19,24 +20,28 @@ export const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = ({ navigatio
     <SafeAreaView style={styles.container}>
       <View style={[COMMON_STYLES.flexContainer, COMMON_STYLES.centeredContainer]}>
         <Line
-          height={18}
+          height={LINE_HEIGHT.MEDIUM}
           isCompleted={false}
           style={{ lineContainer: styles.upperFirstLineContainer }}
         />
         <Line
-          height={18}
+          height={LINE_HEIGHT.MEDIUM}
           isCompleted={false}
           style={{ lineContainer: styles.upperSecondLineContainer }}
         />
         <Text style={styles.deityFlexionTitle}>{'Deity\nFlexion'}</Text>
       </View>
       <View style={COMMON_STYLES.flexContainer}>
-        <Line height={12} isCompleted={false} style={{ lineContainer: styles.line1 }} />
-        <Line height={18} style={{ lineContainer: COMMON_STYLES.mb18 }} />
+        <Line
+          height={LINE_HEIGHT.SMALL}
+          isCompleted={false}
+          style={{ lineContainer: styles.line1 }}
+        />
+        <Line height={LINE_HEIGHT.MEDIUM} style={{ lineContainer: COMMON_STYLES.mb18 }} />
 
         <TouchableOpacity onPress={onRegisterPress}>
           <Line
-            height={64}
+            height={LINE_HEIGHT.BUTTON}
             style={{
               lineContainer: [COMMON_STYLES.centeredContainer, COMMON_STYLES.mb18],
             }}
@@ -46,7 +51,7 @@ export const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = ({ navigatio
         </TouchableOpacity>
         <TouchableOpacity onPress={onLoginPress}>
           <Line
-            height={64}
+            height={LINE_HEIGHT.BUTTON}
             style={{
               lineContainer: [COMMON_STYLES.centeredContainer, COMMON_STYLES.mb18],
             }}
@@ -55,8 +60,12 @@ export const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = ({ navigatio
           </Line>
         </TouchableOpacity>
 
-        <Line height={18} style={{ lineContainer: styles.line3 }} />
-        <Line height={12} isCompleted={false} style={{ lineContainer: styles.line4 }} />
+        <Line height={LINE_HEIGHT.MEDIUM} style={{ lineContainer: styles.line3 }} />
+        <Line
+          height={LINE_HEIGHT.SMALL}
+          isCompleted={false}
+          style={{ lineContainer: styles.line4 }}
+        />
       </View>
     </SafeAreaView>
   );
